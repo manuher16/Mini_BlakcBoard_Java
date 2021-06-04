b63d5c7322a59bd436ff2dea14a9509f3768c6d3
manuher16 <73207879+manuher16@users.noreply.github.com>
2021-06-03 22:51:53 -0600

    actulizar script

--- Mini_BlackBoard/src/main/webapp/scripts.js
+++ Mini_BlackBoard/src/main/webapp/scripts.js
@@ -1,7 +1,57 @@
 /**
  * 
  */
-
+//Registro=1
+//Maestro=2
+//ALumno=3
+const user=[
+	{
+	username:'userR1',
+	password:'111111',
+	role:1
+},
+	{
+	username:'userR2',
+	password:'111111',
+	role:1
+},
+	{
+	username:'userR3',
+	password:'111111',
+	role:1
+},
+	{
+	username:'userM1',
+	password:'111111',
+	role:2
+},
+	{
+	username:'userM2',
+	password:'111111',
+	role:2
+},
+	{
+	username:'userA3',
+	password:'111111',
+	role:2
+}
+,
+	{
+	username:'userMA1',
+	password:'111111',
+	role:3
+},
+	{
+	username:'userA2',
+	password:'111111',
+	role:3
+},
+	{
+	username:'userA3',
+	password:'111111',
+	role:3
+},
+]
 function redirect(url){
 	location.href=url;
 }
